3140abdf49e2e23a49ce9c062e224ba802be47d0
HasenschwandtnerAndreas <113516687+HasenschwandtnerAndreas@users.noreply.github.com>
2025-07-19 18:01:32 +0200

    implemented navigation bar

--- ParaLensApp/App.tsx
+++ ParaLensApp/App.tsx
@@ -1,28 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import { StatusBar, useColorScheme } from "react-native";
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
+import HistoryScreen from './HistoryScreen';
+import CameraScreen from './CameraScreen';
+import SettingsScreen from './SettingsScreen';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+const Tab = createBottomTabNavigator();
 
-function App() {
+export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
+    <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: isDarkMode ? '#181818' : '#fff',
+            borderTopWidth: 0,
+            elevation: 10,
+            height: 60,
+          },
+            // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({ focused, color}) => {
+            let iconName = '';
+            if (route.name === 'History') iconName = 'clock';
+            else if (route.name === 'Camera') iconName = 'camera';
+            else if (route.name === 'Settings') iconName = 'settings';
+            return <Icon name={iconName} size={28} color={focused ? '#4F8EF7' : color || '#888'} />;
+          },
+          tabBarActiveTintColor: '#4F8EF7',
+          tabBarInactiveTintColor: isDarkMode ? '#aaa' : '#888',
+        })}
+      >
+        <Tab.Screen name="History" component={HistoryScreen} />
+        <Tab.Screen name="Camera" component={CameraScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
