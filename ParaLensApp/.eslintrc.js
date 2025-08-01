58597d8249678922e86876ad4cd05b6d921d9c42
reschii <alexander@resch-web.at>
2025-08-01 12:28:47 +0200

    init template

--- ParaLensApp/.eslintrc.js
+++ ParaLensApp/.eslintrc.js
@@ -1,4 +1,61 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'jest'],
+  rules: {
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-shadow': 'off',
+    'no-undef': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-return-await': 'off',
+    'no-nested-ternary': 'off',
+    'no-plusplus': 'off',
+    'no-unused-vars': 'off',
+    'import/no-unresolved': 'off',
+    'prettier/prettier': 'error',
+    'no-use-before-define': 'off',
+    'react/function-component-definition': [
+      2,
+      {namedComponents: 'arrow-function'},
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {extensions: ['.js', '.ts', '.jsx', '.tsx']},
+    ],
+    'react/prop-types': 'off',
+    'import/extensions': [
+      'error',
+      {extensions: ['.js', '.ts', '.jsx', '.tsx']},
+    ],
+    'import/prefer-default-export': 'off',
+    'jest/expect-expect': [
+      'off',
+      {assertFunctionNames: ['expect', 'fc.assert']},
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+      },
+    },
+  },
 };
--- ParaLensApp/babel.config.js
+++ ParaLensApp/babel.config.js
@@ -1,8 +1,33 @@
+const path = require('path');
+
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['react-native-worklets-core/plugin'],
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@root': path.resolve(__dirname),
+          '@': path.resolve(__dirname, 'src'),
+          '@components': path.resolve(__dirname, 'src', 'components'),
+          '@hooks': path.resolve(__dirname, 'src', 'hooks'),
+          '@app': path.resolve(__dirname, 'src', 'app'),
+          '@features': path.resolve(__dirname, 'src', 'features'),
+          '@types': path.resolve(__dirname, 'src', 'types'),
+          '@utils': path.resolve(__dirname, 'src', 'utils'),
+          '@config': path.resolve(__dirname, 'src', 'config'),
+          '@lib': path.resolve(__dirname, 'src', 'lib'),
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: './.env',
+      },
+    ],
   ],
 };
